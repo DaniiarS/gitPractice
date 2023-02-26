9ab47afaea5371589860e96e741be9d3a12d926d
Daniiar Suiunbekov <daniyar.suyunbekov@gmail.com>
2023-02-26 17:49:51 +0600

    Some changes were made to the second file

--- my_file2.py
+++ my_file2.py
@@ -1,1 +1,3 @@
-print('This is a second file created on GitHub repository')
+print('This is a second file created in the GitHub repository')
+
+print('These are the changes made to the second file crearted in the GitHub repository')
